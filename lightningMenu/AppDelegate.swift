8d11d51441e1f0c5702790d0a72755346c422bde
zaru <sakuraba@basicinc.jp>
2020-03-28 10:11:15 +0900

    Initial Commit

---
+++ lightningMenu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  lightningMenu
+//
+//  Created by zaru on 2020/03/28.
+//  Copyright © 2020年 zaru. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
