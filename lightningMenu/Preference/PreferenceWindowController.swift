41a2a5650407eab0e90c77501cda14deadbc0821
zaru <zarutofu@gmail.com>
2020-04-03 11:00:48 +0900

    add temporary view controllers

--- lightningMenu/Preference/PreferenceWindowController.swift
+++ lightningMenu/Preference/PreferenceWindowController.swift
@@ -4,7 +4,34 @@ class PreferenceWindowController: NSWindowController {
 
     static let sharedController = PreferenceWindowController(windowNibName: NSNib.Name("PreferenceWindow"))
 
+    @IBOutlet weak var toolbar: NSToolbar!
+    var tabs: [NSViewController] = []
+
     override func windowDidLoad() {
         super.windowDidLoad()
+
+        tabs = [
+            SettingsViewController(nibName: NSNib.Name("Settings"), bundle: nil),
+            ShortkeyViewController(nibName: NSNib.Name("Shortkey"), bundle: nil)
+        ]
+
+        switchTab(0)
+    }
+
+    @IBAction func selectMenu(_ sender: NSToolbarItem) {
+        print("selectMenu")
+        switchTab(sender.tag)
+    }
+
+    func switchTab(_ index: Int) {
+        removeViews()
+        let viewController = tabs[index]
+        window?.contentView?.addSubview(viewController.view)
+    }
+
+    func removeViews() {
+        window?.contentView?.subviews.forEach { view in
+            view.removeFromSuperview()
+        }
     }
 }
---
+++ lightningMenu/Preference/Tabs/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  lightningMenu
+//
+//  Created by zaru on 2020/04/03.
+//  Copyright © 2020 zaru. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: NSViewController {
+    override func loadView() {
+        super.loadView()
+    }
+}
