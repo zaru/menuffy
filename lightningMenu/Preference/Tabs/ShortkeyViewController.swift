934ae5fa42fb19638eef163e648a91c62210241f
zaru <zarutofu@gmail.com>
2020-04-04 07:38:30 +0900

    add recordview

--- lightningMenu/Preference/Tabs/ShortkeyViewController.swift
+++ lightningMenu/Preference/Tabs/ShortkeyViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import Cocoa
+import KeyHolder
+import Magnet
 
 class ShortkeyViewController: NSViewController {
+    @IBOutlet weak var mainShortkeyRecordView: RecordView!
+
     override func loadView() {
         super.loadView()
+        mainShortkeyRecordView.delegate = self
+        let keyCombo = KeyCombo(keyCode: 46, carbonModifiers: 4352)
+        mainShortkeyRecordView.keyCombo = keyCombo
     }
+}
+
+extension ShortkeyViewController: RecordViewDelegate {
+    func recordViewShouldBeginRecording(_ recordView: RecordView) -> Bool {
+        return true
+    }
+
+    func recordView(_ recordView: RecordView, canRecordKeyCombo keyCombo: KeyCombo) -> Bool {
+        return true
+    }
+
+    func recordViewDidClearShortcut(_ recordView: RecordView) {
+    }
+
+    func recordView(_ recordView: RecordView, didChangeKeyCombo keyCombo: KeyCombo) {
+        print(keyCombo)
+    }
+
+    func recordViewDidEndRecording(_ recordView: RecordView) {
+    }
+
 }
